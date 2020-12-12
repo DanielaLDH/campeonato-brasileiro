3355a7637ae345452eb1d11009a0308301474828
Daniela Luiza <danii.bnu@hotmail.com>
2020-12-12 00:58:46 -0300

    first commit

---
+++ src/views/Inicio.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-3">Brasileirão Série A</h2>
+    <v-img 
+    class="inicio-imagem mb-4"
+    src="https://media.istockphoto.com/photos/feet-of-soccer-player-tread-on-soccer-ball-for-kickoff-in-the-stadium-picture-id992892836" />
+    <p>Sed accumsan gravida lorem et condimentum. Vivamus sit amet lectus feugiat, porta justo ut, elementum ex. Integer a vestibulum arcu. In vulputate consequat massa eget scelerisque. Pellentesque justo diam, tempor eu consequat sed, placerat a lectus. Ut porttitor gravida placerat. Proin id porta nisi. Duis finibus sit amet ante quis semper. Aliquam imperdiet interdum arcu, et eleifend urna. </p>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Inicio'
+}
+</script>
+
+<style scoped>
+.inicio-imagem{
+    border-radius: 5px;
+}
+</style>
